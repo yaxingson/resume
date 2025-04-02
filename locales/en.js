1710527c0aed0f044dc90944ab3892a3f291ad95
soetas <3228891558@qq.com>
2025-04-02 20:57:04 +0800

    refactor: update files

---
+++ locales/en.js
@@ -0,0 +1,12 @@
+export default {
+  translation:{
+    summary:'summary',
+    projects:'projects',
+    education:'education',
+    languages:'languages',
+    'work experience':'work experience',
+    'skill stacks':'skill stacks',
+    'awards':'awards',
+
+  }
+}
